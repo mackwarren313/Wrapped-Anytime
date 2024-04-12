6783dee046675cf5b7c9e991659f5907a8f32e5b
vdamineni <vdamineni3@gatech.edu>
2024-04-11 21:04:53 -0400

    made a helper method createUser

--- app/src/main/java/com/example/wrappedanytime/firebasewrapper/FirestoreDatabase.java
+++ app/src/main/java/com/example/wrappedanytime/firebasewrapper/FirestoreDatabase.java
@@ -2,6 +2,8 @@ package com.example.wrappedanytime.firebasewrapper;
 
 import com.example.wrappedanytime.firebasewrapper.data.UserEntry;
 import com.example.wrappedanytime.firebasewrapper.data.WrappedDataEntry;
+import com.example.wrappedanytime.spotify.Datatypes.User;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 // Note: Should be declared a static instance
@@ -12,5 +14,13 @@ public class FirestoreDatabase {
     public static final FirestoreCollection<WrappedDataEntry> WRAPPED_COLLECTION = new FirestoreCollection<>(db.collection("wraps"));
 //    public static final FirestoreCollection<UserData> TEST_USER_COLLECTION = new FirestoreCollection<>(db.collection("test-users"));
 //    public static final FirestoreCollection<WrappedData> TEST_WRAPPED_COLLECTION = new FirestoreCollection<>(db.collection("test-wraps"));
-
+    public static void createUser (FirebaseUser fbUser, User spotifyUser){
+        FirestoreDatabase.USER_COLLECTION.putItem(
+                new UserEntry(
+                        spotifyUser.getDisplayName(),
+                        fbUser.getUid(),
+                        spotifyUser.getId()
+                )
+        );
+    }
 }
